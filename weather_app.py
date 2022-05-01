eb13487fcdf9eb71c0dfa4213658566c6f8c6dea
Mike <michael.pursell@gmail.com>
2022-05-01 09:03:15 +0100

    added Twilio client, organised code into classes and smaller functions

--- weather_app.py
+++ weather_app.py
@@ -1,8 +1,40 @@
-from audioop import reverse
+from itsdangerous import json
 import requests
 import os
 from dotenv import load_dotenv, find_dotenv
 from datetime import date, timedelta
+from twilio.rest import Client
+
+class TwilioTextMessage:
+    
+    def __init__(self, messageText):
+        load_dotenv(find_dotenv())
+        self.accountSID = os.environ.get('TWILIO_ACCOUNT_SID')
+        self.authToken = os.environ.get('TWILIO_AUTH_TOKEN')
+        self.twilioPhoneNumber = os.environ.get('TWILIO_PHONE_NUMBER')
+        self.myPhoneNumber = os.environ.get('MY_PHONE_NUMBER')
+        self.messageText = messageText
+
+    def send_message(self):
+        self.twilioClient = Client(self.accountSID, self.authToken)
+
+        self.message = self.twilioClient.messages.create(
+            to=self.myPhoneNumber,
+            from_=self.twilioPhoneNumber,
+            body=self.messageText
+        )
+
+class ApiHandler:
+    
+    def __init__(self, url):
+        load_dotenv(find_dotenv())
+        self.url = url
+        self.apiKey = os.environ.get("API_KEY")
+    
+    def response(self):
+        apiCall = requests.get(f"{self.url}{self.apiKey}")
+        return apiCall
+
 
 
 def GetTomorrowFormattedDateAsString():
@@ -14,16 +46,22 @@ def GetTomorrowFormattedDateAsString():
     formattedDate = f"{tomorrow}Z"
     return str(formattedDate)
 
+def GetWeather():
 
+    weatherCheck = ApiHandler("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/310015?res=3hourly&key=")
+    weatherJson = weatherCheck.response().json()
+
+    return weatherJson
+    
 def GetWindGusts():
 
-    load_dotenv(find_dotenv())
-    apiKey = os.environ.get("API_KEY")
+    # load_dotenv(find_dotenv())
+    # apiKey = os.environ.get("API_KEY")
 
-    nottinghamWeather = requests.get(
-        f"http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/310015?res=3hourly&key={apiKey}"
-    )
-    nottinghamWeatherJson = nottinghamWeather.json()
+    # nottinghamWeather = requests.get(
+    #     f"http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/310015?res=3hourly&key={apiKey}"
+    # )
+    nottinghamWeatherJson = GetWeather()
 
     periodList = nottinghamWeatherJson["SiteRep"]["DV"]["Location"]["Period"]
 
@@ -33,12 +71,11 @@ def GetWindGusts():
     for period in periodList:
         if period["value"] == tomorrow:
             for report in period["Rep"]:
-                # G here is the predicted wind gust speeds as string, so they
+                # G here is the predicted wind gust speeds as string,
                 # need to be cast to int for sorting / min-maxxing
                 gustSpeedReports.append(int(report["G"]))
 
     maxGustSpeed = max(gustSpeedReports)
-    # print(maxGustSpeed)
 
     if maxGustSpeed > 30:
         return maxGustSpeed
@@ -46,4 +83,16 @@ def GetWindGusts():
         return 0
 
 
-GetWindGusts()
+def checkWeather():
+
+    if GetWindGusts() >= 30:
+        textMessage = TwilioTextMessage('Wind Over 30mph tomorrow.  Take down the Bball hoop!')
+        textMessage.send_message()
+    else:
+        textMessage = TwilioTextMessage('No wind gusts')
+        textMessage.send_message()
+
+
+checkWeather()
+
+
